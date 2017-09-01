2ade6fc069e723424b905fa52c0f925c9bd91937
Oracle <oracle@Caseys-MacBook-Pro.local>
2017-09-01 15:52:37 -0500

    scheduler

---
+++ js/components/schedule/index.js
@@ -0,0 +1,136 @@
+import React, { Component } from "react";
+import {
+  Container,
+  Header,
+  ActionSheet,
+  Title,
+  Content,
+  Button,
+  Icon,
+  Badge,
+  Text,
+  Left,
+  Right,
+  Body,
+  Card,
+  CardItem,
+  Thumbnail
+} from "native-base";
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import styles from "./styles";
+
+const logo = require("../../../img/contacts/varun.png");
+const cardImage = require("../../../img/drawer-cover.png");
+
+var BUTTONS = ["8:00am", "9:00am", "10:00am", "11:00am", "12:00am", "1:00pm",
+              "2:00pm", "3:00pm", "4:00pm", "5:00pm", "6:00pm", "7:00pm", "8:00pm", "Cancel"];
+var DESTRUCTIVE_INDEX = 13;
+var CANCEL_INDEX = 13;
+
+class Schedule extends Component {
+  //eslint-disable-line
+
+  render() {
+    return (
+      <Container style={styles.container}>
+        <Header>
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.navigate("DrawerOpen")}
+            >
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Schedule</Title>
+          </Body>
+          <Right />
+        </Header>
+
+        <Content padder>
+          <Card style={styles.mb}>
+            <CardItem>
+              <Left>
+                <Thumbnail source={logo} />
+                <Body>
+                  <Text>Joey Smith</Text>
+                  <Text note>San Francisco, CA</Text>
+                </Body>
+              </Left>
+            </CardItem>
+
+            <CardItem cardBody>
+
+            </CardItem>
+            <CardItem style={{ paddingVertical: 0 }}>
+              <Left>
+                <Button iconLeft transparent>
+                  <Icon active name="thumbs-up" />
+                  <Text>48 Meetings</Text>
+                </Button>
+              </Left>
+              <Body>
+                <Button iconLeft transparent>
+                  <Icon active name="chatbubbles" />
+                  <Text>2 Associates</Text>
+                </Button>
+              </Body>
+              <Right>
+                <Text>18h ago</Text>
+              </Right>
+            </CardItem>
+          </Card>
+
+
+
+
+          <Calendar
+            // Initially visible month. Default = Date()
+            current={'2017-09-01'}
+            // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+            minDate={'2016-05-10'}
+            // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+            maxDate={'2018-04-30'}
+            // Handler which gets executed on day press. Default = undefined
+            onDayPress={(day) => {
+              ActionSheet.show(
+                {
+                  options: BUTTONS,
+                  cancelButtonIndex: CANCEL_INDEX,
+                  destructiveButtonIndex: DESTRUCTIVE_INDEX,
+                  title: day.dateString
+                },
+                buttonIndex => {
+                  this.setState({ clicked: BUTTONS[buttonIndex] });
+                }
+              )
+
+              console.log('selected day', day)
+            }}
+            // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+            monthFormat={'yyyy MM'}
+            // Handler which gets executed when visible month changes in calendar. Default = undefined
+            onMonthChange={(month) => {console.log('month changed', month)}}
+            // Hide month navigation arrows. Default = false
+            hideArrows={true}
+            // Replace default arrows with custom ones (direction can be 'left' or 'right')
+            renderArrow={(direction) => (<Arrow />)}
+            // Do not show days of other months in month page. Default = false
+            hideExtraDays={true}
+            // If hideArrows=false and hideExtraDays=false do not swich month when tapping on greyed out
+            // day from another month that is visible in calendar page. Default = false
+            disableMonthChange={true}
+            // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+            firstDay={1}
+          />
+
+        </Content>
+
+
+      </Container>
+    );
+  }
+}
+
+export default Schedule;
---
+++ js/components/schedule/styles.js
@@ -0,0 +1,17 @@
+const React = require("react-native");
+
+const { StyleSheet } = React;
+
+export default {
+  container: {
+    backgroundColor: "#FFF",
+    height: 300
+  },
+  mb: {
+    marginBottom: 0
+  },
+  card: {
+    backgroundColor: "#888",
+    height: 100
+  }
+};
